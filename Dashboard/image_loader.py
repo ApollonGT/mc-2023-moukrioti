dd87a4fec42417c2e292a0efa28b9db18c0b7b48
elisavetm <elis.moukrioti@mc-class.gr>
2024-05-15 15:01:40 +0300

    new

--- Dashboard/image_loader.py
+++ Dashboard/image_loader.py
@@ -2,18 +2,21 @@ import streamlit as st
 import base64
 
 def render_image(filepath: str):
-   """
-   Render image in Streamlit from file.
+    """
+    Render image in Streamlit from file.
 
-   Parameters:
-   filepath (str): Path to the image file.
+    Parameters:
+    filepath (str): Path to the image file.
 
-   Returns:
-   None
-   """
-   mime_type = filepath.split('.')[-1:][0].lower()
-   with open(filepath, "rb") as f:
-       content_bytes = f.read()
-       content_b64encoded = base64.b64encode(content_bytes).decode()
-       image_string = f'data:image/{mime_type};base64,{content_b64encoded}'
-       st.image(image_string)
+    Returns:
+    None
+    """
+    try:
+        mime_type = filepath.split('.')[-1].lower()
+        with open(filepath, "rb") as f:
+            content_bytes = f.read()
+            content_b64encoded = base64.b64encode(content_bytes).decode()
+            image_string = f'data:image/{mime_type};base64,{content_b64encoded}'
+            st.image(image_string)
+    except Exception as e:
+        st.error(f"Error: {e}")
